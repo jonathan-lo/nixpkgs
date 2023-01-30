93644470b97deb440c9590366baa5bc9003e596e
Jonathan Lo <jchl027@gmail.com>
2023-01-30 20:55:31 +1100

    tweak apply

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."jlo@DESKTOP-7RRDPPB" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."DESKTOP-7RRDPPB" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
