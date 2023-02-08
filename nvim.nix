760c00b02297aed363befb2fd364420b2945e744
Jonathan Lo <jchl027@gmail.com>
2023-02-08 21:27:32 +1100

    toggle nvim-tree with alt-1

--- nvim.nix
+++ nvim.nix
@@ -162,7 +162,9 @@
 
       {
         config = ''
+          local api = require("nvim-tree.api")
           require("nvim-tree").setup()
+          vim.keymap.set('n', '<A-1>', api.tree.toggle, opts)
           '';
 
         plugin = nvim-tree-lua;
