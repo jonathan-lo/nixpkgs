20eade8fd82345bab7146d59a39ea54482954e16
Jonathan Lo <jlo@tyro.com>
2023-03-22 08:06:06 +1100

    Revert "dodgy hack for os detection"
    This reverts commit 531e4b5d298024f29180c160f124f16eb2538837.

--- lib/modules.nix
+++ lib/modules.nix
@@ -7,8 +7,7 @@ let
     id mapAttrsToList filterAttrs hasPrefix hasSuffix nameValuePair removeSuffix
     toLower systems;
   inherit (systems.elaborate { system = builtins.currentSystem; }) uname;
-  #os = toLower uname.system;
-  os = "darwin";
+  os = toLower uname.system;
 in
 rec {
   mapModules = dir: fn:
