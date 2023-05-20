e71e3c52e65353ea26edd5684fb1d32c3336fb1f
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2023-05-20 17:06:35 +1000

    set nvim leader key to space

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -8,7 +8,7 @@
     enable = true;
 
     extraConfig = ''
-      map <leader> "\<Space>"
+      let mapleader = "\<Space>"
       nnoremap <C-J> <C-W><C-J>
       nnoremap <C-K> <C-W><C-K>
       nnoremap <C-L> <C-W><C-L>
@@ -179,10 +179,10 @@
           local opts = {buffer=bufnr, noremap=true, silent=true}
           vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
