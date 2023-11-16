2717d11e1495d3ca4cd081650b3c6ef0a2dfc983
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2023-11-16 19:43:41 +1100

    linting

--- private/darwin.nix
+++ private/darwin.nix
@@ -3,9 +3,9 @@
 {
   home = {
     sessionVariables = {
-     DOCKER_HOST= "unix://$HOME/.docker/docker.sock";
-     REQUESTS_CA_BUNDLE = "/Library/Certificates/allcerts.pem";
-     NIX_SSL_CERT_FILE = "/Library/Certificates/allcerts.pem";
+      DOCKER_HOST = "unix://$HOME/.docker/docker.sock";
+      REQUESTS_CA_BUNDLE = "/Library/Certificates/allcerts.pem";
+      NIX_SSL_CERT_FILE = "/Library/Certificates/allcerts.pem";
     };
   };
 }
