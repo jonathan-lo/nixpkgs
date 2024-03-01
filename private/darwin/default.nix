a44bc900a603e9e231d5876aadc202576863b8cc
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2024-03-01 13:37:18 +1100

    remove DOCKER_HOST customisation

--- private/darwin/default.nix
+++ private/darwin/default.nix
@@ -3,7 +3,6 @@
 {
   home = {
     sessionVariables = {
-      DOCKER_HOST = "unix://$HOME/.docker/docker.sock";
       REQUESTS_CA_BUNDLE = "/Library/Certificates/allcerts.pem";
       NIX_SSL_CERT_FILE = "/Library/Certificates/allcerts.pem";
     };
