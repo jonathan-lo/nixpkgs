126157f578f2c58f6e9a6a1f0b6b95e266af1fba
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2024-10-15 09:14:12 +1100

    node extra ca certs

--- private/darwin/default.nix
+++ private/darwin/default.nix
@@ -5,6 +5,7 @@
     sessionVariables = {
       REQUESTS_CA_BUNDLE = "/Library/Certificates/allcerts.pem";
       NIX_SSL_CERT_FILE = "/Library/Certificates/allcerts.pem";
+      NODE_EXTRA_CA_CERTS = "/Library/Certificates/allcerts.pem";
     };
   };
 }
