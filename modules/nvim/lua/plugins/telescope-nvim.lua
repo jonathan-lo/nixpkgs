ed426ee8f101c1db6b9a781f45268e16fee8412e
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2024-10-30 17:05:46 +1100

    fmt

--- modules/nvim/lua/plugins/telescope-nvim.lua
+++ modules/nvim/lua/plugins/telescope-nvim.lua
@@ -1,15 +1,14 @@
 return {
-
-{
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    {
-      "nvim-telescope/telescope-fzf-native.nvim"
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim"
+      },
     },
-  },
-  keys = {
-    { "rg", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
-    { "<C-n>", "<cmd>Telescope find_files<cr>", desc = "Find files (Root Dir)" }
+    keys = {
+      { "rg", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
+      { "<C-n>", "<cmd>Telescope find_files<cr>", desc = "Find files (Root Dir)" }
+    }
   }
-}
 }
