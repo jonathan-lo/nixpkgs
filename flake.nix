e80780bebe0fc63b4e93820c93e6d97ccdcd8e8f
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2025-01-12 20:48:28 +1100

    update flake to 24.11 channel

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     darwin = {
@@ -9,7 +9,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
