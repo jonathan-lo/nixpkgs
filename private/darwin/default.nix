f1c85b2a77b8bebe82edc64a58210a3f95d56166
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2025-04-01 09:53:16 +1100

    remove CA certs override

--- private/darwin/default.nix
+++ private/darwin/default.nix
@@ -1,11 +1,4 @@
 { config, pkgs, ... }:
 
 {
-  home = {
-    sessionVariables = {
-      REQUESTS_CA_BUNDLE = "/Library/Certificates/allcerts.pem";
-      NIX_SSL_CERT_FILE = "/Library/Certificates/allcerts.pem";
-      NODE_EXTRA_CA_CERTS = "/Library/Certificates/allcerts.pem";
-    };
-  };
 }
