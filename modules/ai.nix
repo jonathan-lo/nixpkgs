e031b269f25dedb9f8abc42075ab09ca9f43ec7a
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2025-10-16 11:08:56 +1100

    add ideavimrc

--- modules/ai.nix
+++ modules/ai.nix
@@ -4,6 +4,7 @@
   home.packages = with pkgs; [
     unstable.claude-code
     lazygit
+    unstable.uv # for running mcps
   ];
 
   home.file.".claude/settings.json".text = ''{
--- modules/editor.nix
+++ modules/editor.nix
@@ -5,5 +5,6 @@
     exercism
     gitbutler
     helix
+    vscode
   ];
 }
