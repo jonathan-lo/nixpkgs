6310daf730b9cb5968a6dd80ba3048095dcc1ad3
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-01-18 11:30:24 +1100

    adds docker daemon on budu

--- hosts/linux/budu/configuration.nix
+++ hosts/linux/budu/configuration.nix
@@ -130,6 +130,7 @@
     isNormalUser = true;
     description = "jonathan";
     extraGroups = [
+      "docker"
       "networkmanager"
       "wheel"
     ];
@@ -159,6 +160,10 @@
     protonvpn-gui
     unixtools.ifconfig
   ];
+
+  virtualisation.docker = {
+    enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
