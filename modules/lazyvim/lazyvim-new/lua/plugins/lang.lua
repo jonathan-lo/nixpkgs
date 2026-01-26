c4edbb7282de3c6d88a5950695b2d5c5f2d04bc6
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-01-26 21:45:06 +1100

    disable ktlint

--- modules/lazyvim/lazyvim-new/lua/plugins/lang.lua
+++ modules/lazyvim/lazyvim-new/lua/plugins/lang.lua
@@ -1,16 +1,26 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      yamlls = {
-        settings = {
-          yaml = {
-            format = {
-              enable = false
-            }
-          }
-        }
-      }
-    }
-  }
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = {
+          settings = {
+            yaml = {
+              format = {
+                enable = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        kotlin = {}, -- disable ktlint formatting
+      },
+    },
+  },
 }
