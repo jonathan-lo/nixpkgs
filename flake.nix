a1cb4adef81ec7f171f3a498147c1777fb5e8eeb
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-02-17 22:21:15 +1100

    start migration to flake-parts

--- flake.nix
+++ flake.nix
@@ -12,103 +12,116 @@
 
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    import-tree = {
+      url = "github:vic/import-tree";
+      flake = false;
     };
   };
 
   outputs =
     inputs@{
-      self,
       nixpkgs,
-      nixpkgs-unstable,
       catppuccin,
       darwin,
       determinate,
+      flake-parts,
       home-manager,
       ...
     }:
-    let
-      overlay = final: prev: {
-        unstable = import inputs.nixpkgs-unstable {
-          system = prev.stdenv.hostPlatform.system;
-          config.allowUnfree = true;
-        };
-      };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
 
-      overlays = [ overlay ];
+      flake =
+        let
+          overlay = final: prev: {
+            unstable = import inputs.nixpkgs-unstable {
+              system = prev.stdenv.hostPlatform.system;
+              config.allowUnfree = true;
+            };
+          };
 
-      nixPkgsConfig = {
-        inherit overlays;
-        config.allowUnfree = true;
-      };
-    in
-    {
-      nixosConfigurations = {
-        "budu" = nixpkgs.lib.nixosSystem {
+          overlays = [ overlay ];
 
-          specialArgs = { inherit inputs; };
+          nixPkgsConfig = {
+            inherit overlays;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          nixosConfigurations = {
+            "budu" = nixpkgs.lib.nixosSystem {
 
-          modules = [
-            determinate.nixosModules.default
-            home-manager.nixosModules.home-manager
-            {
-              nixpkgs = nixPkgsConfig;
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.jlo = {
-                imports = [
-                  ./hosts/linux/budu/home.nix
-                  catppuccin.homeModules.catppuccin
-                ];
-              };
-            }
-            ./hosts
-            ./hosts/linux/budu/configuration.nix
-          ];
-        };
-      };
+              specialArgs = { inherit inputs; };
 
-      darwinConfigurations."Jonathans-MacBook-Pro" = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-
-        specialArgs = { inherit inputs; };
-
-        modules = [
-          home-manager.darwinModules.home-manager
-          {
-            nixpkgs = nixPkgsConfig;
-            home-manager.useGlobalPkgs = true;
-            home-manager.users.jlo = {
-              imports = [
-                ./hosts/darwin/nc/home.nix
-                catppuccin.homeModules.catppuccin
+              modules = [
+                determinate.nixosModules.default
+                home-manager.nixosModules.home-manager
+                {
+                  nixpkgs = nixPkgsConfig;
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users.jlo = {
+                    imports = [
+                      ./hosts/linux/budu/home.nix
+                      catppuccin.homeModules.catppuccin
+                    ];
+                  };
+                }
+                ./hosts
+                ./hosts/linux/budu/configuration.nix
               ];
             };
-          }
-          ./hosts
-          ./hosts/darwin/homebrew.nix
-          ./hosts/darwin/services.nix
-          ./hosts/darwin/settings.nix
-        ];
-      };
+          };
 
-      homeConfigurations."DESKTOP-7RRDPPB" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [
-          ./home.nix
-          ./hosts/linux/settings.nix
-          catppuccin.homeModules.catppuccin
-        ];
-      };
-      homeConfigurations."LAPTOP-GIVRN79I" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [
-          ./home.nix
-          ./hosts/linux/settings.nix
-          catppuccin.homeModules.catppuccin
-        ];
-      };
+          darwinConfigurations."Jonathans-MacBook-Pro" = darwin.lib.darwinSystem {
+            system = "aarch64-darwin";
+
+            specialArgs = { inherit inputs; };
+
+            modules = [
+              home-manager.darwinModules.home-manager
+              {
+                nixpkgs = nixPkgsConfig;
+                home-manager.useGlobalPkgs = true;
+                home-manager.users.jlo = {
+                  imports = [
+                    ./hosts/darwin/nc/home.nix
+                    catppuccin.homeModules.catppuccin
+                  ];
+                };
+              }
+              ./hosts
+              ./hosts/darwin/homebrew.nix
+              ./hosts/darwin/services.nix
+              ./hosts/darwin/settings.nix
+            ];
+          };
+
+          homeConfigurations."DESKTOP-7RRDPPB" = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages."x86_64-linux";
+            modules = [
+              ./home.nix
+              ./hosts/linux/settings.nix
+              catppuccin.homeModules.catppuccin
+            ];
+          };
+          homeConfigurations."LAPTOP-GIVRN79I" = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages."x86_64-linux";
+            modules = [
+              ./home.nix
+              ./hosts/linux/settings.nix
+              catppuccin.homeModules.catppuccin
+            ];
+          };
+        };
+
+      perSystem = { pkgs, ... }: { };
     };
 }
