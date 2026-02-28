4375d54bda86db3316831a9b8c8654852ee01990
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-02-28 17:26:30 +1100

    clean up migrated modules

--- _legacy-modules/bat.nix
+++
@@ -1,6 +0,0 @@
-{ ... }:
-
-{
-  programs.bat.enable = true;
-  # theme is set by catppuccin module in theme.nix
-}
--- _legacy-modules/btop.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  programs.btop = {
-    enable = true;
-  };
-}
