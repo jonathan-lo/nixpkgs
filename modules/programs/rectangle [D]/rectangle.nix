fffdc7522b08e5cdcd194d5b043068eed278ee12
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-03-03 21:33:56 +1100

    launch rectangle on startup

--- modules/programs/rectangle [D]/rectangle.nix
+++ modules/programs/rectangle [D]/rectangle.nix
@@ -4,5 +4,13 @@
     environment.systemPackages = with pkgs; [
       rectangle
     ];
+
+    launchd.user.agents.rectangle = {
+      serviceConfig = {
+        ProgramArguments = [ "${pkgs.rectangle}/Applications/Rectangle.app/Contents/MacOS/Rectangle" ];
+        RunAtLoad = true;
+        KeepAlive = false;
+      };
+    };
   };
 }
