09e63d9d7bd0f335a54c413eb9106e7b5d6da24f
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-03-09 21:50:02 +1100

    extract import for hardware config

--- modules/hosts/linux [N]/budu/configuration.nix
+++ modules/hosts/linux [N]/budu/configuration.nix
@@ -17,9 +17,6 @@
           printing
           gpu
           steam
-        ]
-        ++ [
-          ./_hardware-configuration.nix
         ];
 
       # Bootloader
---
+++ modules/hosts/linux [N]/budu/flake-parts.nix
@@ -0,0 +1,13 @@
+
+{ inputs, ... }:
+{
+  flake.modules.nixos.budu =
+    { pkgs, ... }:
+    {
+      imports =
+        with inputs.self.modules.nixos;
+        [
+          ./_hardware-configuration.nix
+        ];
+    };
+}
