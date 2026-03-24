54cb55ca42fecb1ba9c9d28b377928e0f8b3f694
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-03-24 22:04:02 +1100

    fix broken direnv package after flake update

--- modules/programs/cli/direnv.nix
+++ modules/programs/cli/direnv.nix
@@ -1,12 +1,17 @@
 { inputs, ... }:
 {
   flake.modules.homeManager.direnv =
-    { ... }:
+    { pkgs, ... }:
     {
       programs.direnv = {
         enable = true;
         enableZshIntegration = true;
         nix-direnv.enable = true;
+        # direnv's Makefile passes -linkmode=external which requires cgo,
+        # but the nixpkgs derivation sets CGO_ENABLED=0, causing the build to fail.
+        package = pkgs.direnv.overrideAttrs (old: {
+          env = (old.env or { }) // { CGO_ENABLED = "1"; };
+        });
       };
     };
 }
---
+++ modules/system/02-system-cli/system-cli.nix
@@ -0,0 +1,38 @@
+{
+  inputs,
+  ...
+}:
+{
+  # import all essential nix-tools which are used in all modules of a specific class
+
+  flake.modules.nixos.system-cli = {
+    imports =
+      with inputs.self.modules.nixos;
+      [
+        system-minimal
+        home-manager
+      ]
+  };
+
+  flake.modules.darwin.system-cli = {
+    imports =
+      with inputs.self.modules.darwin;
+      [
+        system-minimal
+        determinate
+        home-manager
+        homebrew
+      ]
+  };
+
+  # impermanence is not added by default to home-manager, because of missing Darwin implementation
+  # for linux home-manager stand-alone configurations it has to be added manualy
+
+  flake.modules.homeManager.system-cli = {
+    imports =
+      with inputs.self.modules.homeManager;
+      [
+        system-minimal
+      ]
+  };
+}
