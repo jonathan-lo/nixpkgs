56ef03a14c2be806f50b59e3487d396ff7e40d98
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-03-30 16:57:13 +1100

    install github/speckit

--- flake.nix
+++ flake.nix
@@ -1,32 +1,31 @@
 # DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
-
   outputs =
     inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ((import inputs.import-tree) ./modules);
 
   inputs = {
     catppuccin.url = "github:catppuccin/nix";
     darwin = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:lnl7/nix-darwin/nix-darwin-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     determinate = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-file.url = "github:vic/flake-file";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree = {
-      flake = false;
       url = "github:vic/import-tree";
+      flake = false;
     };
+    llm-agents.url = "github:numtide/llm-agents.nix";
     nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
   };
-
 }
---
+++ modules/programs/cli/ai [nd]/flake-parts.nix
@@ -0,0 +1,8 @@
+{
+  ...
+}:
+{
+  flake-file.inputs = {
+    llm-agents.url = "github:numtide/llm-agents.nix";
+  };
+}
