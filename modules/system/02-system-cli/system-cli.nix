e28b69c27b7611946bb11674405f6797ccf78f82
Jonathan Lo <3763897+jonathan-lo@users.noreply.github.com>
2026-03-31 08:33:17 +1100

    lint

--- modules/system/02-system-cli/system-cli.nix
+++ modules/system/02-system-cli/system-cli.nix
@@ -6,33 +6,27 @@
   # import all essential nix-tools which are used in all modules of a specific class
 
   flake.modules.nixos.system-cli = {
-    imports =
-      with inputs.self.modules.nixos;
-      [
-        system-minimal
-        home-manager
-      ];
+    imports = with inputs.self.modules.nixos; [
+      system-minimal
+      home-manager
+    ];
   };
 
   flake.modules.darwin.system-cli = {
-    imports =
-      with inputs.self.modules.darwin;
-      [
-        system-minimal
-        determinate
-        home-manager
-        homebrew
-      ];
+    imports = with inputs.self.modules.darwin; [
+      system-minimal
+      determinate
+      home-manager
+      homebrew
+    ];
   };
 
   # impermanence is not added by default to home-manager, because of missing Darwin implementation
   # for linux home-manager stand-alone configurations it has to be added manualy
 
   flake.modules.homeManager.system-cli = {
-    imports =
-      with inputs.self.modules.homeManager;
-      [
-        system-minimal
-      ];
+    imports = with inputs.self.modules.homeManager; [
+      system-minimal
+    ];
   };
 }
